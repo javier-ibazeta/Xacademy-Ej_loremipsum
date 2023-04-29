49d2b7e8c40fc781cb3e6c378cd11d57f1e30377
javier-ibazeta <72580634+javier-ibazeta@users.noreply.github.com>
2023-04-29 11:27:10 -0300

    Update contar.sh
    comentarios extras

--- contar.sh
+++ contar.sh
@@ -3,11 +3,16 @@
 for i in {1..5}
 do
 	#devolver la cantidad de líneas de cada archivo
-	_lineas=$(wc -l loremipsum-$i.txt | cut -d ' ' -f1 )
+	cant_Lineas=$(wc -l loremipsum-$i.txt | cut -d ' ' -f1 )
 	#mostrar el nombre del archivo y la cantidad de líneas que tiene
 	echo loremipsum-$i.txt tiene $_lineas líneas
 done
 
+# con el comando cut doy formato de salida correcto, corto el nombre del archivo que sale por segunda vez
+# el comando wc opción -l devuelve la cantidad de lineas que tiene un archivo lo asigno a la variable cant_Lineas
+# y lo muestro por pantalla con el comando echo 
+# no pude obtener una salida más elegante o quizas el comando wc no me lo permita hacer de otra manera 
+# salida sin el comando cut
 #loremipsum-1.txt tiene 15 loremipsum-1.txt líneas
-#loremipsum-1.txt tiene 15 loremipsum-1.txt líneas
+
 
